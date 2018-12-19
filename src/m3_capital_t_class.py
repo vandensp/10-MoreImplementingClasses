250962c3ffc2befe8802e6837d2ec3ce927b608e
vandensp <vandensp@rose-hulman.edu>
2018-12-19 09:14:14 -0500

    Done

--- src/m3_capital_t_class.py
+++ src/m3_capital_t_class.py
@@ -22,8 +22,8 @@ def main():
 
     #run_test_simple_t()
     #run_test_set_colors()
-    run_test_move_by()
-    # run_test_clone()
+    #run_test_move_by()
+    run_test_clone()
 
 
 def run_test_simple_t():
@@ -325,6 +325,7 @@ class CapitalT(object):
         # IMPORTANT RESTRICTION: You are NOT permitted to add any instance
         # variables beyond  h_rect  and  v_rect, at any point of this exercise.
         #######################################################################
+
 
 
 # -----------------------------------------------------------------------------
